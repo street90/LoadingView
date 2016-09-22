80e85f611d0aba4f170aed471781ddc1d4d36415
SongLang <justsonglang@gmail.com>
2016-09-22 22:57:34 +0800

    first commit

---
+++ loading/src/main/java/com/song/loading/CircleLoading.java
@@ -0,0 +1,23 @@
+package com.song.loading;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by song on 2016/9/22.
+ */
+
+public class CircleLoading extends View {
+    public CircleLoading(Context context) {
+        super(context);
+    }
+
+    public CircleLoading(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleLoading(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
